df6ed77234433653796f627c83cfa715bfd2e703
elliotmc02 <elliotmoyanocutler2002@gmail.com>
2023-11-02 23:25:14 +0100

    Formulario productos

--- Tienda/productos_form.php
+++ Tienda/productos_form.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Formulario productos</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <?php require "DATABASE/db_tienda.php" ?>
 </head>
 
 <body>
@@ -52,6 +53,37 @@
                 }
             }
         }
+
+        // * Comprobar precio
+        if (strlen($temp_precio) == 0) {
+            $err_precio = "El precio es obligatorio";
+        } else {
+            if (!filter_var($temp_precio, FILTER_VALIDATE_FLOAT)) {
+                $err_precio = "El precio debe ser un numero";
+            } else {
+                if ($temp_precio < 0) {
+                    $err_precio = "El precio no puede ser negativo";
+                } else if ($temp_precio > 99999.99) {
+                    $err_precio = "El precio no puede ser mayor de 99999.99";
+                } else {
+                    $precio = $temp_precio;
+                }
+            }
+        }
+
+        // * Comprobar cantidad
+        if (strlen($temp_cantidad) == 0) {
+            $err_cantidad = "La cantidad es obligatoria";
+        } elseif (filter_var($temp_cantidad, FILTER_VALIDATE_INT) === false) { // Otra forma de hacerlo, el ctype_digit no acepta negativos mientras que el filter_var si, pero el 0 te devuelve FALSE ya que el "0" se evalua como FALSE. Una forma de arreglar esto es comprobando estrictamente como he hecho
+            // } elseif (!ctype_digit($temp_cantidad)) {
+            $err_cantidad = "La cantidad debe ser un número entero";
+        } elseif ($temp_cantidad < 0) {
+            $err_cantidad = "La cantidad no puede ser negativa";
+        } elseif ($temp_cantidad > 99999) {
+            $err_cantidad = "La cantidad no puede ser mayor de 99999";
+        } else {
+            $cantidad = $temp_cantidad;
+        }
     }
     ?>
 
@@ -61,23 +93,37 @@
             <div class="mb-3">
                 <label class="form-label">Nombre del producto:</label>
                 <input type="text" name="nombre" class="form-control">
-                <?php if (isset($err_nombre)) echo "<label style='color: red;'>" . $err_nombre . "</label>" ?>
+                <?php if (isset($err_nombre)) echo "<label class='text-danger'>" . $err_nombre . "</label>" ?>
             </div>
             <div class="mb-3">
                 <label class="form-label">Descripcion del producto: </label>
                 <input type="text" name="descripcion" class="form-control">
+                <?php if (isset($err_descripcion)) echo "<label class='text-danger'>" . $err_descripcion . "</label>" ?>
             </div>
-            <div class="col-md-3">
+            <div class="col-md-3 mb-3">
                 <label class="form-label">Precio: </label>
                 <input type="text" name="precio" class="form-control">
+                <?php if (isset($err_precio)) echo "<label class='text-danger'>" . $err_precio . "</label>" ?>
             </div>
-            <div class="col-md-3">
+            <div class="col-md-3 mb-3">
                 <label class="form-label">Cantidad:</label>
                 <input type="text" name="cantidad" class="form-control">
+                <?php if (isset($err_cantidad)) echo "<label class='text-danger'>" . $err_cantidad . "</label>" ?>
             </div>
-            <button class="btn btn-primary" type="submit">Enviar</button>
+            <input class="btn btn-primary" type="submit" value="Enviar">
         </form>
     </div>
+
+    <?php
+    if (isset($nombre) && isset($descripcion) && isset($precio) && isset($cantidad)) {
+        $sql = "INSERT INTO productos (nombreProducto, precio, descripcion, cantidad) VALUES ('$nombre', '$precio', '$descripcion', '$cantidad')";
+        if ($conexion->query($sql)) {
+            echo "Producto añadido correctamente";
+        } else {
+            echo "Error: " . $sql . "<br>" . $conexion->error;
+        }
+    }
+    ?>
 </body>
 
 </html>
