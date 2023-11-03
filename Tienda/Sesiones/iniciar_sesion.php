0baaf10d81155e1caedcf14e29481b4b8fcd5f77
elliotmc02 <elliotmoyanocutler2002@gmail.com>
2023-11-03 14:46:09 +0100

    Cosas nuevas

---
+++ Tienda/Sesiones/iniciar_sesion.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Iniciar sesion</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+    <?php require "../DATABASE/db_login.php" ?>
+</head>
+
+<body>
+    <?php
+    function depurar($entrada)
+    {
+        return trim(htmlspecialchars($entrada));
+    }
+
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $usuario = $_POST["usuario"];
+        $contrasena = $_POST["contrasena"];
+
+        $sql = "SELECT * FROM usuarios WHERE usuario = '$usuario'";
+        // ' OR 1=1 -- '
+        $resultado = $conexion->query($sql);
+
+        if ($resultado->num_rows === 0) {
+        ?>
+            <div class="alert alert-danger" role="alert">
+                El usuario no existe
+            </div>
+        <?php
+        } else {
+            while ($fila = $resultado->fetch_assoc()) {
+                $contrasena_cifrada = $fila["contrasena"];
+            }
+
+            $acceso_valido = password_verify($contrasena, $contrasena_cifrada);
+
+            if ($acceso_valido) {
+                echo "Inicio de sesion correcto";
+                session_start();
+                $_SESSION["usuario"] = $usuario;
+                header('location: ../index.php');
+            } else {
+                echo "Contraseña incorrecta";
+            }
+        }
+    }
+    ?>
+    <div class="container">
+        <h1>Iniciar sesion</h1>
+        <form action="" method="post">
+            <div class="mb-3">
+                <label class="form-label">Usuario:</label>
+                <input class="form-control" type="text" name="usuario">
+            </div>
+            <div class="mb-3">
+                <label class="form-label">Contraseña:</label>
+                <input class="form-control" type="password" name="contrasena">
+            </div>
+            <input class="btn btn-primary" type="submit" value="Iniciar sesion">
+        </form>
+    </div>
+</body>
+
+</html>
---
+++ Tienda/Sesiones/registro.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Registrarse</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+    <?php require "../DATABASE/db_login.php" ?>
+</head>
+
+<body>
+    <?php
+    function depurar($entrada)
+    {
+        return trim(htmlspecialchars($entrada));
+    }
+
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $usuario = $_POST["usuario"];
+        $contrasena = $_POST["contrasena"];
+
+        $contrasena_cifrada = password_hash($contrasena, PASSWORD_DEFAULT);
+
+        $sql = "INSERT INTO usuarios VALUES ('$usuario', '$contrasena_cifrada')";
+
+        $conexion->query($sql);
+    }
+    ?>
+    <div class="container">
+        <h1>Registrarse</h1>
+        <form action="" method="post">
+            <div class="mb-3">
+                <label class="form-label">Usuario:</label>
+                <input class="form-control" type="text" name="usuario">
+            </div>
+            <div class="mb-3">
+                <label class="form-label">Contraseña:</label>
+                <input class="form-control" type="password" name="contrasena">
+            </div>
+            <input class="btn btn-primary" type="submit" value="Registrarse">
+        </form>
+    </div>
+</body>
+
+</html>
